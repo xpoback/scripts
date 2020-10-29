9dedb5a2c35631e94afa0baecb515edf11e6fdd0
xpoback <xpoback@gmail.com>
2020-10-29 17:40:51 +0100

    Create enable-xdebug.sh

---
+++ enable-xdebug.sh
@@ -0,0 +1,21 @@
+# check if sudo
+if [ ! ${EUID:-$(id -u)} -eq 0 ]; then
+  >&2 echo "This command must be run under sudo permissions";
+  exit 1;
+fi;
+
+PHP_VERSION=$1
+
+if [ ! $PHP_VERSION ]; then
+  >&2 echo "PHP version must be specified as first argument";
+  exit 1;
+fi;
+
+# uncomment xdebug
+sed -i -E 's/[#]+//' /etc/php/$PHP_VERSION/cli/conf.d/20-xdebug.ini
+sed -i -E 's/[#]+//' /etc/php/$PHP_VERSION/fpm/conf.d/20-xdebug.ini
+service php$PHP_VERSION-fpm restart
+export XDEBUG_CONFIG="remote_enable=1 remote_mode=req remote_port=9000 remote_host=127.0.0.1 remote_connect_back=0"
+echo "XDebug for php$PHP_VERSION enabled"
+
+exit 0;
